35a7b13777900f1e04640befbc8a57c48ba5c010
Lukes2233 <matteogorno096@gmail.com>
2025-12-18 20:58:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-document.addEventListener("DOMContentLoaded" , function(){
-    let file = document.getElementById("file");
 
-    file.textContent = "Sto prelevando tutti i dati dal tuo disco";
-})
