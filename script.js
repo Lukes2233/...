67a9929b332f840fb27c194f63e72336089a6613
Lukes2233 <matteogorno096@gmail.com>
2025-12-18 18:59:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded" , function(){
+    let file = document.getElementById("file");
+
+    file.textContent = "Sto prelevando tutti i dati dal tuo disco";
+})
